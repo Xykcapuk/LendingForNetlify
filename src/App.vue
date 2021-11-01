ee3dd6685c981e25104a80042961bae49f376583
Xykcapuk <73579539+Xykcapuk@users.noreply.github.com>
2021-11-01 16:36:56 +0300

    Change text

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="wrapper">
+    <Header></Header>
+    <div class="text">
+      <span>Моя любовь</span>
+    </div>
   </div>
 </template>
 
 <script>
 
+import Header from "./components/Header";
 export default {
   name: 'App',
-  components: {}
+  components: {Header}
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  height: 100vh;
+}
+.wrapper {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  flex-direction: column;
+}
+.text {
+  margin-top: 30px;
+  font-size: 40px;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <h1>Кисонькааа</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.header {
+
+}
+</style>
